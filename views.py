f1955d5443cc3ae14b16340d07a810b1928a0cb8
Joe Cera <subscribeto@runbox.com>
2019-07-10 14:34:37 -0700

    attempting to display blog entries within blog page

--- views.py
+++ views.py
@@ -8,18 +8,30 @@ blog_posts = [
 			"title": "Disroot.org",
 			"date": "June 24, 2019",
 			"hook" : "disroot",
+			"intro" : """
+				Have you ever been interested in using common tools but with a little more privacy?
+				Then you decide you don't want to put in the work to find and host different tools. What if a group 
+				was already doing that for you?
+  			""",
 		},
 		{
 			"filename": "blog/elevator.html",
 			"title": "Spotted in an elevator",
 			"date": "June 01, 2019",
 			"hook" : "elevator",
+			"intro" : """
+				During a recent stay at a hotel I spotted something curious in the elevator.
+			"""
 		},
 		{
 			"filename": "blog/openwireless.html",
 			"title": "Open Wireless",
 			"date": "June 24, 2019",
 			"hook" : "openwireless",
+			"intro" : """
+				Have you ever been stranded somewhere and you need an internet connection?
+				You and a lot of other people. Do you like organizations like Electronic Frontier Foundation, mozilla, Internet Archive?
+			""",
 		},
 		
 	]		
